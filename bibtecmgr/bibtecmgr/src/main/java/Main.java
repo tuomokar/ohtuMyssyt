bd42d7a188a1c37dd843db6e7defd57108f51a0d
Lindström Olli-Pekka <oplindst@da5-cs-b221-09.helsinki.fi>
2016-04-08 19:01:59 +0300

    Viitemanagerin tiedostonkirjoitus

--- bibtecmgr/bibtecmgr/src/main/java/Main.java
+++ bibtecmgr/bibtecmgr/src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Artikkeli artikkeli1 = new Artikkeli("testeri", "testeri", "testeri", "testeri", 2016);
+        System.out.println("test");
     }
 }
