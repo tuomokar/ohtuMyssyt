88c693e7c221002a9049577ad6cdb4167dc881d7
Guest <guest-z5AeFS@da5-cs-b221-06.helsinki.fi>
2016-04-08 18:31:48 +0300

    FileManager

--- bibtecmgr/bibtecmgr/src/main/java/Main.java
+++ bibtecmgr/bibtecmgr/src/main/java/Main.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Artikkeli artikkeli1 = new Artikkeli("testeri", "testeri", "testeri", "testeri", 2016);
     }
