e3c6ba55af927688de5ee6c8e57a091eacd50d2b
Törnroos Topi <topi@da3-cs-dk110-13.helsinki.fi>
2016-04-19 18:28:45 +0300

    pieni muutos testiin

--- bibtecmgr/bibtecmgr/src/test/java/ArtikkeliTest.java
+++ bibtecmgr/bibtecmgr/src/test/java/ArtikkeliTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.*;
  */
 public class ArtikkeliTest {
     
+    protected Artikkeli artikkeli;
+    
     public ArtikkeliTest() {
     }
     
@@ -30,6 +32,12 @@ public class ArtikkeliTest {
     
     @Before
     public void setUp() {
+        String journal = "Commun. ACM";
+        String title = "Novice mistakes: are the folk wisdoms correct?";
+        String author = "Spohrer, James C. and Soloway, Elliot";
+        String publisher = "Consortium for Computing Sciences in Colleges";
+        int year = 2006;
+        Artikkeli artikkeli = new Artikkeli(journal, title, author, publisher, year);
     }
     
     @After
@@ -41,12 +49,6 @@ public class ArtikkeliTest {
      */
     @Test
     public void testGetJournal() {
-        String journal = "Commun. ACM";
-        String title = "Novice mistakes: are the folk wisdoms correct?";
-        String author = "Spohrer, James C. and Soloway, Elliot";
-        String publisher = "Consortium for Computing Sciences in Colleges";
-        int year = 2006;
-        Artikkeli artikkeli = new Artikkeli(journal, title, author, publisher, year);
         assertEquals(artikkeli.getJournal(), "Commun. ACM");
     }
     
