6fc6231babe1d919c4bcc9f60656f604033c46da
kjh <kalle.hiljanen@helsinki.fi>
2016-04-22 12:13:15 +0300

    poistettu kommentteja

--- src/test/java/com/myssyt/bibtex/ViiteTest.java
+++ src/test/java/com/myssyt/bibtex/ViiteTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.myssyt.bibtex;
 
 import org.junit.After;
@@ -12,10 +7,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author omena
- */
 public class ViiteTest {
     
     public ViiteTest() {
